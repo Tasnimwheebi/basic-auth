e5d7a3f90b3893a42a19ef3dc7a759de5f727dda
Tasnimwheebi <tasnimwheebi@gmail.com>
2021-06-01 18:05:50 +0300

    fix the test and add uml

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,27 +1,29 @@
 'use strict';
 const base64 = require('base-64');
-const Users = require('../router.js');
 const bcrypt = require('bcrypt');
-async function middleware (req,res,next){
-  let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'sdkjdsljd=']
-  let encodedString = basicHeaderParts.pop();  // sdkjdsljd=
-  let decodedString = base64.decode(encodedString); // "username:password"
-  let [username, password] = decodedString.split(':'); // username, password   
-  
-  try {
-    const user = await Users.findOne({ username });
-    const valid = await bcrypt.compare(password, user.password);
-    if (valid) {
-    //   res.status(200).json(user);
+const Users = require('../models/users-model.js');
+
+
+module.exports= async (req,res,next)=>{
+  const encoded = req.headers.authorization.split(' ').pop();
+  const decoded = base64.decode(encoded);
+  const [username,password]= decoded.split(':');
+  try{
+    
+   
+    const user = await Users.findOne({username});
+    const isValid = await bcrypt.compare(password,user.password);
+    if (isValid){
+      console.log('user',user);
+     
       req.user=user;
+      
+      
       next();
+    } else {
+      res.status(401).json({error:'Invalid user'});
     }
-    else {
-      throw new Error('Invalid User');
-    }
-  } catch (error) { res.status(403).send('Invalid Login'); }
-
-  next();
-}
-
-module.exports= middleware;
+  } catch (error) {
+    res.status(401).json({error:error.message});
+  }
+};
--- src/auth/models/users-model.js
+++ src/auth/models/users-model.js
@@ -3,18 +3,21 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 // Create a mongoose model
-const usersSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-});
-const Users = mongoose.model('users', usersSchema);
 
-usersSchema.pre('save', async function(next) {
+
+const userSchema = new mongoose.Schema({
+  username : {type : String , require : true , unique :true},
+  password : {type :String , require : true},
+});
+
+
+userSchema.pre('save', async function(next) {
   const hash = await bcrypt.hash(this.password,10);
   this.password = hash;
   next();
 });
 
-module.exports = Users;
+const User = mongoose.model('User',userSchema);
 
+module.exports = User;
 
